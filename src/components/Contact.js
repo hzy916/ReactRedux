a75c3a641fd43e99ba120cca7e2a2c708b3739a3
ziyun <zh29@st-andrews.ac.uk>
2018-12-15 18:32:26 +0000

    react using axios to get post data

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = (props) => {
+    console.log(props);
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Contact</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,24 @@
+const initState = {
+    posts:[
+
+    {"id": 1,
+    "title": "sun11111henderit",
+    "body": "quia et susci11111pit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {"id": 2,
+    "title": "sunt aut facnderit",
+    "body": "pit\nsuscipit33333 recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {"id": 3,
+    "title": "sunt aut facere repellat o reprehenderit",
+    "body": "quiansequunt22222ur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    }
+
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer
